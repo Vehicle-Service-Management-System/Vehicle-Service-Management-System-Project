a582fb616500e6f621fe6c30ef574129d00c6464
Kasi-ab <kasia15b6@gmail.com>
2025-09-23 19:03:03 +0530

    oop

---
+++ src/model/Customer.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    public Customer(int id, String name, String email, String phoneNumber, String address ){
+        this.id =id;
+        this.name=name;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.address=address;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+    }
+    public void setAddress(String address){
+        this.address=address;
+    }
+
+}
